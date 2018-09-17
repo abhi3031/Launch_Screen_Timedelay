b13f77e9f3b130bec414b3e3cf85533bc3944911
Abhishek3031 <patelabhishek301@gmail.com>
2018-09-17 10:46:16 +0530

    Appdelegate
    Launch Screen Time Added

--- Launch_Screen_Timedelay/AppDelegate.swift
+++ Launch_Screen_Timedelay/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+     
+         RunLoop.current.run(until: NSDate(timeIntervalSinceNow: 10) as Date)
         return true
     }
 
